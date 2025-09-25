ba15df11bd281a44bb547c13fa4a30990967fccc
Shyam Anand <sh1408m@gmail.com>
2025-09-25 20:33:41 +0530

    Comment edit

--- chain.js
+++ chain.js
@@ -1,28 +1,9 @@
 const { ChatGroq } = require("@langchain/groq");
-// const { MongoDBChatMessageHistory } = require("langchain/stores/message/mongodb");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 module.exports.llm = new ChatGroq({
     model: "llama-3.3-70b-versatile",
-    // openAIApiKey: process.env.OPENAI_API_KEY,
     temperature: 0.7
   });
-// module.exports.makeChain = function(userId) {
-//   const collection = mongoose.connection.collection("conversations");
 
-//   const chatHistory = new MongoDBChatMessageHistory({
-//     collection,
-//     sessionId: userId
-//   });
-
-//   const memory = new BufferMemory({
-//     chatHistory,
-//     returnMessages: true,
-//     memoryKey: "history"
-//   });
-
- 
-
-//   return new ConversationChain({ llm, memory });
-// }
